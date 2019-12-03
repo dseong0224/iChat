4efefcc337c4b6480cb9d3903ea0ff1ef3016a83
Daniel Seong <dseong0224@gmail.com>
2019-12-03 02:05:56 -0800

    added input field for text message input on change handler

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
-import closeIcon from '../../icons/closeIcon';
-import onlineIcon from '../../icons/onlineIcon';
+import closeIcon from '../../icons/closeIcon.png';
+import onlineIcon from '../../icons/onlineIcon.png';
 
-import './InforBar.css';
+import './InfoBar.css';
 
-const InfoBar = ( { room } ) => {
-  <div className="InfoBar">
+const InfoBar = ({ room }) => (
+  <div className="infoBar">
     <div className="leftInnerContainer">
       <img className="onlineIcon" src={onlineIcon} alt="online image" />
-      <h3>{ room }</h3>
+      <h3>{room}</h3>
     </div>
     <div className="rightInnerContainer">
       <a href="/"><img src={closeIcon} alt="close image" /></a>
     </div>
   </div>
-}
+)
+
+export default InfoBar;
