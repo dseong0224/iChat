0fdb57d86b3cb5b198e33dc8b7a44d444d8d9fab
Daniel Seong <dseong0224@gmail.com>
2019-12-03 12:50:20 -0800

    logic that deals with displaying messages differentiates users message from other users' messages color coded

---
+++ client/src/components/Message/Message.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./Message.css";
+
+const Message = ({ message, name }) => {
+  isSentByCurrentUser = false;
+
+  const trimmedName = name.trim().toLowerCase();
+
+  if (user === trimmedName) {
+    isSentByCurrentUser = true;
+  }
+
+  return isSentByCurrentUser ? (
+    <div className="messageContainer justifyEnd">
+      <p className="sentText pr-10">{trimmedName}</p>
+      <div className="messageBox backgroundBlue">
+        <p className="messageText colorWhite">{text}</p>
+      </div>
+    </div>
+  ) : (
+    <div className="messageContainer justifyStart">
+      <div className="messageBox backgroundLight">
+        <p className="messageText colorDark">{text}</p>
+      </div>
+      <p className="sentText pr-10">{trimmedName}</p>
+    </div>
+  );
+};
+
+export default Message;
