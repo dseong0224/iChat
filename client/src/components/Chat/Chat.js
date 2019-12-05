c0ee16ad9725fbfe09bb427de7d95d052f534c4b
Daniel Seong <dseong0224@gmail.com>
2019-12-04 22:39:31 -0800

    added users component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,15 +3,18 @@ import queryString from 'query-string';
 import io from 'socket.io-client';
 
 import './Chat.css';
+
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
 import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000'
@@ -24,22 +27,28 @@ const Chat = ({ location }) => {
     setName(name);
     setRoom(room);
 
-    socket.emit('join', { name, room }, () => {
-
+    socket.emit('join', { name, room }, (error) => {
+      if(error){
+        alert(error);
+      }
     });
-
-    return () => {
-      socket.emit('disconnect');
-
-      socket.off();
-    }
   }, [ENDPOINT, location.search]);
 
 
   useEffect(() => {
     socket.on('message', (message) => {
       setMessages([...messages, message]);
+    });
+
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
     })
+
+    return () => {
+      socket.emit('disconnect');
+
+      socket.off();
+    }
   }, [messages]);
 
   //function for sending messages
@@ -61,6 +70,7 @@ const Chat = ({ location }) => {
         <Messages messages={messages} name={name}/>
         <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
       </div>
+      <TextContainer users={users}/>
     </div>
   )
 
