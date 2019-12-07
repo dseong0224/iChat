024eefe623c15af25e375f0edb86d2c47d448d88
Daniel Seong <dseong0224@gmail.com>
2019-12-06 23:56:40 -0800

    added custom styling to join page and to chat room

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -10,11 +10,11 @@ const Join = () => {
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <h1 className="heading">Join</h1>
-        <div><input placeholder="Name" className="joinInput" type="text" onChange={ event => setName(event.target.value) } /></div>
+        <h1 className="heading">iChat</h1>
+        <div><input placeholder="User Name" className="joinInput" type="text" onChange={ event => setName(event.target.value) } /></div>
         <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={ event => setRoom(event.target.value) } /></div>
         <Link onClick={event => ( !name || !room ) ? event.preventDefault() : null}  to={`/chat?name=${name}&room=${room}`}>
-          <button className="button mt-20" type="submit">Sign In</button>
+          <button className="button mt-20" type="submit">Join A Room</button>
         </Link>
       </div>
     </div>
--- server/users.js
+++ server/users.js
@@ -1,8 +1,8 @@
 let users = [];
 
 const addUser = ({ id, name, room }) => {
+  console.log(id, name, room)
   //Fantasy Soccer ==> fantasysoccer
-debugger;
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
 
@@ -17,29 +17,28 @@ debugger;
   }
 
   if (existingUser) {
-    return { error: "Username is taken" };
+    return {
+      error: "Username is taken"
+    };
   }
 
   const user = { id, name, room };
 
-  alert('hello')
- 
   users.push(user);
 
   return { user };
 };
 
-const removeUser = (id) => {
-  const index = users.findIndex((user) => user.id === id);
+const removeUser = id => {
+  const index = users.findIndex(user => user.id === id);
 
   if (index !== -1) {
-    console.log(users);
     return users.splice(index, 1)[0];
   }
 };
 
-const getUser = id => users.find((user) => user.id === id);
+const getUser = id => users.find(user => user.id === id);
 
-const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+const getUsersInRoom = room => users.filter(user => user.room === room);
 
 module.exports = { addUser, removeUser, getUser, getUsersInRoom };
